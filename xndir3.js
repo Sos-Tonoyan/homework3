76118b8a9a46f0d289ce2589d6d68ff19f923e37
Sos-Tonoyan <85686282+Sos-Tonoyan@users.noreply.github.com>
2021-12-15 15:09:52 +0400

    Add files via upload

---
+++ xndir3.js
@@ -0,0 +1,16 @@
+function hashvich(m,n,a){
+    if(a == "*"){
+        return m*n
+    }
+    if(a == "/"){
+        return m/n
+    }
+    if(a == "+"){
+        return m+n
+    }
+    if(a=="-"){
+        return m-n
+    }
+}
+
+console.log(hashvich(5,4,"-"))
